32dbd7947ad9ae2d86fcc471564ed83fd30d9ca7
Binwaran <binwaran.m@gmail.com>
2025-04-29 11:21:14 +0800

    feat: add answer question-5 to algorithm skill checkpoint

--- question-5.js
+++ question-5.js
@@ -1,6 +1,5 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer:
-
+Answer: O(n^2) เพราะใช้ Bubble Sort มีการจัดเรียง การสลับตำแหน่งของ Array โดยเปรียบเทียบทีละคู่ และทำซ้ำ ไปจนกว่าจะเรียงเสร็จ
 
 */
